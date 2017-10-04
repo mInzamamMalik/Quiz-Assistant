a817cbb6f99d66f63a50c99d7387ae6d6a22a34b
Inzamam Malik <malikasinger1@gmail.com>
2017-10-05 02:56:30 +0500

    oauth is 50% done

--- functions/src/lib/function_category1.ts
+++ functions/src/lib/function_category1.ts
@@ -6,13 +6,47 @@ var cors = _cors({ origin: true });// set these options appropriately According 
 // see document: https://www.npmjs.com/package/cors#configuration-options
 // true means allow everything
 
-
 // http example
-export const addMessage = functions.https.onRequest((req, res) => {
-    const original = req.query.text;
-    admin.database().ref('/messages').push({ original: original }).then(snapshot => {
-        res.redirect(303, snapshot.ref);
-    });
+export const login = functions.https.onRequest((req, res) => {
+
+    let client_id = req.query.client_id
+    let redirect_uri = req.query.redirect_uri
+    let state = req.query.state
+    let response_type = req.query.response_type
+
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    
+    <body>
+        <p>
+            client_id: ${client_id}
+            <br>
+            redirect_uri: ${redirect_uri}
+            <br>
+            state: ${state}
+            <br>
+            response_type: ${response_type}
+        </p>
+        <p>
+            <input type="text">
+            <input type="text">
+            <button>login</button>
+        </p>
+        <script>
+            setTimeOut(function(){
+                window.location = "https://oauth-redirect.googleusercontent.com/r/inzi-quiz-assistant#access_token=myaccesstoken123&token_type=bearer&state=${state}" ;        
+            },3000)
+        </script>
+    </body>
+    
+    </html>`)
 });
 
 //databse trigger example
