fac2c43d040314f9158bb3a300916d0b725a03d8
Inzamam Malik <malikasinger1@gmail.com>
2017-10-05 17:20:24 +0500

    material included in angular app

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MatInputModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
