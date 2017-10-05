06f2c69ea3b3bf5f39ecdfb053aa6ed1c5b223d6
Inzamam Malik <malikasinger1@gmail.com>
2017-10-05 17:49:04 +0500

    basic routing done on angular app

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdCheckboxModule, MatInputModule } from '@angular/material';
 
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { OauthComponent } from './oauth/oauth.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'oauth', component: OauthComponent },
+
+  // { path: '**', component: AppComponent }
+];
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    OauthComponent
   ],
   imports: [
+    RouterModule.forRoot( //for routing
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule, MdCheckboxModule, MatInputModule
---
+++ web/src/app/oauth/oauth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oauth',
+  templateUrl: './oauth.component.html',
+  styleUrls: ['./oauth.component.css']
+})
+export class OauthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
