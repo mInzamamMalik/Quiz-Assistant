842a9f2490a6f7a512556d967f35f18ea226bb7c
Inzamam Malik <malikasinger1@gmail.com>
2017-10-09 16:28:42 +0500

    custom token login and signup api complete

--- functions/src/db/index.ts
+++ functions/src/db/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
-var defaultApp = admin.initializeApp(functions.config().firebase)
+// var defaultApp = admin.initializeApp(functions.config().firebase) // doesnt work with createCustomToken()
+//Error: createCustomToken() requires a certificate with "private_key" set.
+
+var serviceAccount = require("./serviceAccount.json");
+
+let defaultApp = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://inzi-quiz-assistant.firebaseio.com"
+});
+
 
 export { defaultApp }
-export let auth  = admin.auth();
+export let auth = admin.auth();
 const db = admin.database();
 export default db;
--- functions/src/lib/function_category1.ts
+++ functions/src/lib/function_category1.ts
@@ -31,9 +31,25 @@ export const login = functions.https.onRequest((req, res) => {
         .then(function (userRecord) {
             // See the UserRecord reference doc for the contents of userRecord.
             console.log("Successfully fetched user data:", userRecord.toJSON());
+
+            admin.auth().createCustomToken(userRecord.uid)
+                .then(function (customToken) {
+                    // Send token back to client
+                    console.log("customToken: ", customToken);
+                    res.send({
+                        "uid": userRecord.uid,
+                        "customToken": customToken
+                    })
+                })
+                .catch(function (error) {
+                    console.log("Error creating custom token:", error);
+                    res.send("error: firebase auth ")
+
+                });
         })
         .catch(function (error) {
             console.log("Error fetching user data:", error);
+            res.send("error: firebase auth ")
         });
 
 
@@ -47,3 +63,6 @@ export const login = functions.https.onRequest((req, res) => {
     //     });
 
 });
+
+
+
