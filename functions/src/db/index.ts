3e7bc5857e6fbae7b8c9d3f49ad6cf90bf4748d2
Inzamam Malik <malikasinger1@gmail.com>
2017-10-09 17:59:31 +0500

    custom token bug fix

--- functions/src/db/index.ts
+++ functions/src/db/index.ts
@@ -1,18 +1,29 @@
 import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
+import * as _admin from 'firebase-admin'
+import * as _firebase from "firebase";
 
 // var defaultApp = admin.initializeApp(functions.config().firebase) // doesnt work with createCustomToken()
 //Error: createCustomToken() requires a certificate with "private_key" set.
 
 var serviceAccount = require("./serviceAccount.json");
-
-let defaultApp = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+let admin = _admin.initializeApp({
+    credential: _admin.credential.cert(serviceAccount),
     databaseURL: "https://inzi-quiz-assistant.firebaseio.com"
 });
+export { admin }
+
+let config = {
+    apiKey: "AIzaSyAFOf27clnj39WcgzdSVMYw_smjt_ANRrE",
+    authDomain: "inzi-quiz-assistant.firebaseapp.com",
+    databaseURL: "https://inzi-quiz-assistant.firebaseio.com",
+    projectId: "inzi-quiz-assistant",
+    storageBucket: "inzi-quiz-assistant.appspot.com",
+    messagingSenderId: "190190683842"
+};
+let firebase = _firebase.initializeApp(config);
+export { firebase }
 
 
-export { defaultApp }
 export let auth = admin.auth();
 const db = admin.database();
 export default db;
--- functions/src/lib/function_category1.ts
+++ functions/src/lib/function_category1.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as _cors from 'cors';
-import { auth } from '../db'
+import { auth, firebase } from '../db'
 var cors = _cors({ origin: true });// set these options appropriately According to your case,
 // see document: https://www.npmjs.com/package/cors#configuration-options
 // true means allow everything
@@ -27,32 +27,42 @@ export const signup = functions.https.onRequest((req, res) => {
 });
 export const login = functions.https.onRequest((req, res) => {
 
-    auth.getUserByEmail(req.body.email)
-        .then(function (userRecord) {
-            // See the UserRecord reference doc for the contents of userRecord.
-            console.log("Successfully fetched user data:", userRecord.toJSON());
-
-            admin.auth().createCustomToken(userRecord.uid)
-                .then(function (customToken) {
-                    // Send token back to client
-                    console.log("customToken: ", customToken);
-                    res.send({
-                        "uid": userRecord.uid,
-                        "customToken": customToken
-                    })
-                })
-                .catch(function (error) {
-                    console.log("Error creating custom token:", error);
-                    res.send("error: firebase auth ")
-
-                });
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).catch(function (error) {
+        console.log("Error signin:", error);
+        res.send({
+            error: "wrong pass"
         })
-        .catch(function (error) {
-            console.log("Error fetching user data:", error);
-            res.send("error: firebase auth ")
-        });
+
+    }).then(user => {
+        console.log("user:", user)
+
+        auth.getUserByEmail(req.body.email)
+            .then(function (userRecord) {
+                // See the UserRecord reference doc for the contents of userRecord.
+                console.log("Successfully fetched user data:", userRecord.toJSON());
+
+                admin.auth().createCustomToken(userRecord.uid)
+                    .then(function (customToken) {
+                        // Send token back to client
+                        console.log("customToken: ", customToken);
+                        res.send({
+                            "uid": userRecord.uid,
+                            "customToken": customToken
+                        })
+                    })
+                    .catch(function (error) {
+                        console.log("Error creating custom token:", error);
+                        res.send("error: firebase auth ")
+
+                    });
+            })
+            .catch(function (error) {
+                console.log("Error fetching user data:", error);
+                res.send("error: firebase auth ")
+            });
 
 
+    });
     // / on every request we will verify token like this:
     // auth.verifyIdToken(idToken)
     //     .then(function (decodedToken) {
